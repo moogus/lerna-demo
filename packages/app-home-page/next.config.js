33751302e453f19e93594443b4a5392c68d15154
Darren Corbett <corbsoft@gmail.com>
2021-07-28 08:48:45 +0100

    use transilled modules

--- packages/app-home-page/next.config.js
+++ packages/app-home-page/next.config.js
@@ -1,8 +1,19 @@
 /* eslint-disable */
+const STYLED_ATOM_COMPONENTS = '@lerna-demo/components-styled-atoms'
+const UTILS_TYPES = '@lerna-demo/utils-types'
 
 const debug = process?.env?.NODE_ENV === 'development'
 
-const withTM = require('next-transpile-modules')([], { debug })
+const withTM = require('next-transpile-modules')(
+  [
+    // component packages
+    STYLED_ATOM_COMPONENTS,
+
+    // utility packages
+    UTILS_TYPES,
+  ],
+  { debug }
+)
 
 module.exports = withTM({
   async rewrites() {
@@ -20,6 +31,11 @@ module.exports = withTM({
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
+      // component packages
+      [STYLED_ATOM_COMPONENTS]: require.resolve(STYLED_ATOM_COMPONENTS),
+
+      // utility packages
+      [UTILS_TYPES]: require.resolve(UTILS_TYPES),
     }
     config.module.rules.push({
       test: /\.spec.ts$/,
--- packages/app-product-page/next.config.js
+++ packages/app-product-page/next.config.js
@@ -1,8 +1,19 @@
 /* eslint-disable */
+const STYLED_ATOM_COMPONENTS = '@lerna-demo/components-styled-atoms'
+const UTILS_TYPES = '@lerna-demo/utils-types'
 
 const debug = process?.env?.NODE_ENV === 'development'
 
-const withTM = require('next-transpile-modules')([], { debug })
+const withTM = require('next-transpile-modules')(
+  [
+    // component packages
+    STYLED_ATOM_COMPONENTS,
+
+    // utility packages
+    UTILS_TYPES,
+  ],
+  { debug }
+)
 
 module.exports = withTM({
   basePath: '/product',
@@ -17,6 +28,10 @@ module.exports = withTM({
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
+      [STYLED_ATOM_COMPONENTS]: require.resolve(STYLED_ATOM_COMPONENTS),
+
+      // utility packages
+      [UTILS_TYPES]: require.resolve(UTILS_TYPES),
     }
     config.module.rules.push({
       test: /\.spec.ts$/,
